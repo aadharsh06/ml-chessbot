7f8ce8efe532c6091c72f55b6c796abdd5cc9d8e
Aadharsh Venkat <aadharshvenkat06@gmail.com>
2025-07-16 21:03:16 +0530

    Stockfish vs Stockfish data gen

--- gen_datasets.py
+++ gen_datasets.py
@@ -7,20 +7,17 @@ import threading
 import py_server
 from pickle import dump
 from time import time
+import os
 
 from ctypes import CDLL, POINTER, c_int, c_char_p, c_double
 
 start = time()
 
-engine = ce.SimpleEngine.popen_uci(r".\stockfish\stockfish-windows-x86-64-avx2.exe")
-
-engine.configure ( {
-    "UCI_LimitStrength": True,
-    "UCI_Elo": 1800
-} )
+games = 100
+times = [ 0.1, 0.5, 5, 7 ]
 
 def get_engine_policy ( board ):
-    anz = engine.analyse( board, ce.Limit ( time = 0.1 ), multipv = len ( [ str ( im.return_index ( move.uci() ) ) for move in board.legal_moves ] ) )
+    anz = engine.analyse ( board, ce.Limit ( time = np.random.choice ( times ) ), multipv = len ( [ str ( im.return_index ( move.uci() ) ) for move in board.legal_moves ] ) )
 
     moves = []
     scores = []
@@ -40,7 +37,6 @@ def get_engine_policy ( board ):
 
     return moves, policy
 
-
 HOST = "127.0.0.1"
 PORT = 65432
 
@@ -48,66 +44,68 @@ stop = threading.Event()
 t = threading.Thread ( target = py_server.start_server, args = ( stop, ) )
 t.start()
 
-lib = CDLL ( "./mcts.dll" )
+engine = ce.SimpleEngine.popen_uci ( ".\stockfish\stockfish-windows-x86-64-avx2.exe" )
 
-pi = lib.return_pi
-pi.argtypes = [ c_char_p, c_int ]
-pi.restype  = POINTER ( c_double )
+for g in range ( games ):
 
-board = ch.Board()
-cur_fen = board.fen()
-moves = 0
+    lib = CDLL ( "./mcts.dll" )
 
-x = []
+    pi = lib.return_pi
+    pi.argtypes = [ c_char_p, c_int ]
+    pi.restype  = POINTER ( c_double )
 
-while ( not board.is_game_over() ):
-    # Engine plays frst
-    legal_moves, p_hat = get_engine_policy ( board )
-    legal_moves = [ im.return_index ( i ) for i in legal_moves ]
-    full_phat = im.empty_mv()
-    full_phat[legal_moves] = p_hat
-    x.append ( ( cur_fen, list ( full_phat ), 0 ) )
-    move = board.push_uci ( im.return_move ( int ( np.random.choice ( legal_moves, p = p_hat ) ) ) )
-    moves += 1
+    board = ch.Board()
     cur_fen = board.fen()
-    print ( "\nMoves complete: {}\n".format ( moves ), board, "\n", sep = "" )
-    
-    if ( board.is_game_over() ):
-        break
-    
-    # NN plays second
-    legal_moves = [ im.return_index ( move.uci() ) for move in ch.Board ( cur_fen ).legal_moves ]
-    p_hat = pi ( cur_fen.encode(), moves )
-    p_hat = [ p_hat[i] for i in range ( len ( legal_moves ) ) ]
-    full_phat = im.empty_mv()
-    full_phat[legal_moves] = p_hat
-    x.append ( ( cur_fen, list ( full_phat ), 0 ) )
-    move = board.push_uci ( im.return_move ( int ( np.random.choice ( legal_moves, p = p_hat ) ) ) )
-    cur_fen = board.fen()
-    moves += 1
-    print ( "\nMoves complete: {}\n".format ( moves ), board, "\n", sep = "" )
-    
-print ( "\Game over" )
+    moves = 0
 
-if board.result() == "1-0":
-    white = 1
-elif board.result() == "0-1":
-    white = -1
-else:
-    white = 0
+    x = []
 
-for i in range ( len ( x ) ):
-    if i % 2 == 0:
-        x[i][2] = white
+    while ( not board.is_game_over() ):
+        # Engine plays frst
+        legal_moves, p_hat = get_engine_policy ( board )
+        legal_moves = [ im.return_index ( i ) for i in legal_moves ]
+        full_phat = im.empty_mv()
+        full_phat[legal_moves] = p_hat
+        x.append ( [ cur_fen, list ( full_phat ), 0 ] )
+        move = board.push_uci ( im.return_move ( int ( np.random.choice ( legal_moves, p = p_hat ) ) ) )
+        moves += 1
+        cur_fen = board.fen()
+        print ( "\nGame number: {}, Moves complete: {}\n".format ( g + 1, moves ), board, "\n", sep = "" )
+        
+        if ( board.is_game_over() ):
+            break
+        
+        # Engine plays second
+        legal_moves, p_hat = get_engine_policy ( board )
+        legal_moves = [ im.return_index ( i ) for i in legal_moves ]
+        full_phat = im.empty_mv()
+        full_phat[legal_moves] = p_hat
+        x.append ( [ cur_fen, list ( full_phat ), 0 ] )
+        move = board.push_uci ( im.return_move ( int ( np.random.choice ( legal_moves, p = p_hat ) ) ) )
+        moves += 1
+        cur_fen = board.fen()
+        print ( "\nGame number: {}, Moves complete: {}\n".format ( g + 1, moves ), board, "\n", sep = "" )
+
+    if board.result() == "1-0":
+        white = 1
+    elif board.result() == "0-1":
+        white = -1
     else:
-        x[i][2] = (-1) * white
+        white = 0
 
-with open ( "./training_data/x.pkl", 'wb' ) as f:
-    dump ( x, f )
+    for i in range ( len ( x ) ):
+        if i % 2 == 0:
+            x[i][2] = white
+        else:
+            x[i][2] = (-1) * white
+
+    with open ( "./training_data/x{}.pkl".format ( g ), 'wb' ) as f:
+        dump ( x, f )
+
+    end = time()
+
+    print ( "\Game Complete: {} minutes".format ( (end - start)/60 ) )
 
 stop.set()
 engine.quit()
-
-end = time()
-
-print ( "\nProgram Complete: {} minutes".format ( (end - start)/60 ) )
+print ( "\nProgram Complete" )
